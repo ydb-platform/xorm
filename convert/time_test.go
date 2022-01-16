3d1c9fb761325f98ea2c5493bbb28e0431936784
jixianlqb <jixianlqb@noreply.gitea.io>
2022-01-16 16:36:07 +0800

    对 DATE格式进行转换 (#2093)
    对 DATE格式进行转换
    
    Co-authored-by: laiqiangbin <laiqiangbin@chandashi.com>
    Co-authored-by: Lunny Xiao <xiaolunwen@gmail.com>
    Reviewed-on: https://gitea.com/xorm/xorm/pulls/2093
    Reviewed-by: Lunny Xiao <xiaolunwen@gmail.com>
    Co-authored-by: jixianlqb <jixianlqb@noreply.gitea.io>
    Co-committed-by: jixianlqb <jixianlqb@noreply.gitea.io>

--- convert/time_test.go
+++ convert/time_test.go
@@ -16,6 +16,7 @@ func TestString2Time(t *testing.T) {
 	assert.NoError(t, err)
 
 	var kases = map[string]time.Time{
+		"2021-08-10":                time.Date(2021, 8, 10, 8, 0, 0, 0, expectedLoc),
 		"2021-06-06T22:58:20+08:00": time.Date(2021, 6, 6, 22, 58, 20, 0, expectedLoc),
 		"2021-07-11 10:44:00":       time.Date(2021, 7, 11, 18, 44, 0, 0, expectedLoc),
 		"2021-08-10T10:33:04Z":      time.Date(2021, 8, 10, 18, 33, 04, 0, expectedLoc),
