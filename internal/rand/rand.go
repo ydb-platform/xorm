36eb8f84da0e5d51c67d294a43332159be172f15
datbeohbbh <phongtomfapp@gmail.com>
2023-02-12 12:18:42 +0300

    update rand

--- internal/rand/rand.go
+++ internal/rand/rand.go
@@ -11,10 +11,10 @@ import (
 func RandValue(val reflect.Value) interface{} {
 	switch val.Kind() {
 	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int, reflect.Int64:
-		rand.Seed(time.Now().UnixNano())
+		rand.New(rand.NewSource(time.Now().UnixNano()))
 		return rand.Int63() >> 11
 	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint, reflect.Uint64:
-		rand.Seed(time.Now().UnixNano())
+		rand.New(rand.NewSource(time.Now().UnixNano()))
 		return rand.Uint64() >> 11
 	case reflect.String:
 		return uuid.NewString()
