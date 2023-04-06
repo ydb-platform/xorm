94fcec7f65f87b4a227f2b45b6947247a6f0208d
Alberto Garcia <agarcia@circutor.com>
2023-04-06 21:47:59 +0800

    parse timestamp with milliseconds in tz format (#2246)
    This pr fixes a bug caused when a timestmap in TZ format with milliseconds (`2023-04-05T15:50:48.256816Z` or `2023-04-05T15:50:48.256816+08:00`) is being parsed.
    
    - The bug is happens in the function `String2Time` in `convert/time.go`
    - if the timestamp contains milliseconds a layout with the structure `2006-01-02 15:04:05.` is used to parse it
    - This layout contains an space between the date and the hour, if the timestamp returned by the db contains a `T` the parser fails and an error is returned
    
    This pr adds a check for the `T` and milliseconds in the timestamp, if both conditions are present the date is parsed using the RFC3339 layout
    
    Added test cases to check timestamps in this format
    
    Solves [#2244](https://gitea.com/xorm/xorm/issues/2244)
    
    Reviewed-on: https://gitea.com/xorm/xorm/pulls/2246
    Reviewed-by: Lunny Xiao <xiaolunwen@gmail.com>
    Co-authored-by: Alberto Garcia <agarcia@circutor.com>
    Co-committed-by: Alberto Garcia <agarcia@circutor.com>

--- convert/time.go
+++ convert/time.go
@@ -40,6 +40,13 @@ func String2Time(s string, originalLocation *time.Location, convertedLocation *t
 		}
 		dt = dt.In(convertedLocation)
 		return &dt, nil
+	} else if len(s) >= 21 && s[10] == 'T' && s[19] == '.' {
+		dt, err := time.Parse(time.RFC3339, s)
+		if err != nil {
+			return nil, err
+		}
+		dt = dt.In(convertedLocation)
+		return &dt, nil
 	} else if len(s) >= 21 && s[19] == '.' {
 		var layout = "2006-01-02 15:04:05." + strings.Repeat("0", len(s)-20)
 		dt, err := time.ParseInLocation(layout, s, originalLocation)
--- convert/time_test.go
+++ convert/time_test.go
@@ -16,10 +16,19 @@ func TestString2Time(t *testing.T) {
 	assert.NoError(t, err)
 
 	var kases = map[string]time.Time{
-		"2021-08-10":                time.Date(2021, 8, 10, 8, 0, 0, 0, expectedLoc),
-		"2021-06-06T22:58:20+08:00": time.Date(2021, 6, 6, 22, 58, 20, 0, expectedLoc),
-		"2021-07-11 10:44:00":       time.Date(2021, 7, 11, 18, 44, 0, 0, expectedLoc),
-		"2021-08-10T10:33:04Z":      time.Date(2021, 8, 10, 18, 33, 04, 0, expectedLoc),
+		"2021-08-10":                          time.Date(2021, 8, 10, 8, 0, 0, 0, expectedLoc),
+		"2021-07-11 10:44:00":                 time.Date(2021, 7, 11, 18, 44, 0, 0, expectedLoc),
+		"2021-07-11 10:44:00.999":             time.Date(2021, 7, 11, 18, 44, 0, 999000000, expectedLoc),
+		"2021-07-11 10:44:00.999999":          time.Date(2021, 7, 11, 18, 44, 0, 999999000, expectedLoc),
+		"2021-07-11 10:44:00.999999999":       time.Date(2021, 7, 11, 18, 44, 0, 999999999, expectedLoc),
+		"2021-06-06T22:58:20+08:00":           time.Date(2021, 6, 6, 22, 58, 20, 0, expectedLoc),
+		"2021-06-06T22:58:20.999+08:00":       time.Date(2021, 6, 6, 22, 58, 20, 999000000, expectedLoc),
+		"2021-06-06T22:58:20.999999+08:00":    time.Date(2021, 6, 6, 22, 58, 20, 999999000, expectedLoc),
+		"2021-06-06T22:58:20.999999999+08:00": time.Date(2021, 6, 6, 22, 58, 20, 999999999, expectedLoc),
+		"2021-08-10T10:33:04Z":                time.Date(2021, 8, 10, 18, 33, 04, 0, expectedLoc),
+		"2021-08-10T10:33:04.999Z":            time.Date(2021, 8, 10, 18, 33, 04, 999000000, expectedLoc),
+		"2021-08-10T10:33:04.999999Z":         time.Date(2021, 8, 10, 18, 33, 04, 999999000, expectedLoc),
+		"2021-08-10T10:33:04.999999999Z":      time.Date(2021, 8, 10, 18, 33, 04, 999999999, expectedLoc),
 	}
 	for layout, tm := range kases {
 		t.Run(layout, func(t *testing.T) {
