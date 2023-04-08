c40867e67aad3bcb5055951aadda9ef8283d326a
datbeohbbh <phongtomfapp@gmail.com>
2023-04-08 08:16:34 +0300

    fix convert time

--- convert/time.go
+++ convert/time.go
@@ -8,7 +8,6 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
-	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -50,20 +49,10 @@ func String2Time(s string, originalLocation *time.Location, convertedLocation *t
 		dt = dt.In(convertedLocation)
 		return &dt, nil
 	} else if len(s) >= 21 && s[19] == '.' {
-		var dt time.Time
-		var err error
-		re := regexp.MustCompile(`^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$`)
-		if re.MatchString(s) { // is RFC3339Nano
-			dt, err = time.ParseInLocation(time.RFC3339Nano, s, originalLocation)
-			if err != nil {
-				return nil, err
-			}
-		} else {
-			var layout = "2006-01-02 15:04:05." + strings.Repeat("0", len(s)-20)
-			dt, err = time.ParseInLocation(layout, s, originalLocation)
-			if err != nil {
-				return nil, err
-			}
+		var layout = "2006-01-02 15:04:05." + strings.Repeat("0", len(s)-20)
+		dt, err := time.ParseInLocation(layout, s, originalLocation)
+		if err != nil {
+			return nil, err
 		}
 		dt = dt.In(convertedLocation)
 		return &dt, nil
