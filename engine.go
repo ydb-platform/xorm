181cf7ffa7d8e93dc67e2a0c23a96457ab1ad90d
datbeohbbh <phongtomfapp@gmail.com>
2023-05-11 10:14:15 +0300

    fix

--- engine.go
+++ engine.go
@@ -842,7 +842,7 @@ func (engine *Engine) dumpTables(ctx context.Context, tables []*schemas.Table, w
 									return err
 								}
 							} else if yqlType == "Interval" {
-								// !datbeohbbh! TODO: only work if database represent interval time in microsecond. Need more research.
+								// !datbeohbbh! TODO: only work if database represent interval time in microsecond.
 								d, err := strconv.ParseInt(s.String, 10, 64)
 								if err != nil {
 									return err
