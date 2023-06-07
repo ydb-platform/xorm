d0bdc8f98f35c0014de5eb65eb2e1b92d4cc93e1
datbeohbbh <phongtomfapp@gmail.com>
2023-06-07 13:14:41 +0300

    refactor: remove internal *sql.DB

--- session.go
+++ session.go
@@ -189,6 +189,9 @@ func (session *Session) Tx() *core.Tx {
 }
 
 func (session *Session) getQueryer() core.Queryer {
+	if session.engine.dialect.URI().DBType == schemas.YDB {
+		return session.db()
+	}
 	if session.tx != nil {
 		return session.tx
 	}
